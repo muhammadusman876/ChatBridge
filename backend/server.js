0163163477f7865d251b61d7935bd0546f7c3da0
Usman Sameer Khan <usmankhan87015@gmail.com>
2024-06-18 22:27:15 +0200

    New Changes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
-const express = require("express")
+const express = require("express");
 const path = require("path");
 const dotenv = require("dotenv");
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
 
 const authRoutes = require("./routes/auth.js");
-const messageRoutes = require("./routes/message.js")
+const messageRoutes = require("./routes/message.js");
 const userRoutes = require("./routes/user.js");
 
 const connectToMongoDB = require("./db/connectToMongoDB.js");
@@ -12,20 +12,19 @@ const { app, server } = require("./socket/socket.js");
 
 const port = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
 
 dotenv.config();
-
-
 
 //Middlewares
 //Will be used to parse the incoming requests with JSON payloads (from req.body)
 //will help extract fields from req.body
 app.use(express.json());
-app.use(cookieParser()) // to parse the incoming cookies from req.cookies
+app.use(cookieParser()); // to parse the incoming cookies from req.cookies
 
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",messageRoutes);
-app.use("/api/users",userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 //with app.use and get we are able to run our frontend from server
 // this is used to serve static files such as html css js image file sound file etc
@@ -33,20 +32,24 @@ app.use("/api/users",userRoutes);
 //we basically going into __dirname(which root) then frontend / dist
 // app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-// //any routes other than mentioned above 
+// //any routes other than mentioned above
 // app.get("*", (req, res) => {
 //     res.sendFile(path.join(__dirname,"..", "frontend", "dist", "index.html"))
 // })
 
+// app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "..", "frontend", "dist", "index.html"));
+// });
 
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "..", "frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "frontend", "dist", "index.html"));
 });
-
 //this is coming from socket server
-server.listen(port,() => {
-    connectToMongoDB();
-    console.log(`Server Running on port ${port}`);
+server.listen(port, () => {
+  connectToMongoDB();
+  console.log(`Server Running on port ${port}`);
 });
--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -2,49 +2,52 @@ import { io } from "socket.io-client";
 import { useAuthContext } from "./AuthContext";
 import { createContext, useState, useEffect, useContext } from "react";
 
-export const SocketContext = createContext()
+export const SocketContext = createContext();
 
 export const useSocketContext = () => {
-	return useContext(SocketContext);
-}
+  return useContext(SocketContext);
+};
 
 export const SocketContextProvider = ({ children }) => {
-    const [socket, setSocket] = useState(null)
-    const [onlineUsers, setOnlineUsers] = useState([]);    
-    const {authUser} = useAuthContext()  
-    
-    useEffect(() => {
-        //here if we have a authenticated user
-      if (authUser) {
-        //then we will create a connection with io method
-        const socket = io("http://localhost:8000", {
-          //when we are logged in we would like to take userId, for that we that as an argument 
-          query: {
-             userId : authUser._id
-          }
-        })
+  const [socket, setSocket] = useState(null);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const { authUser } = useAuthContext();
 
-        //once the connection is completed, set the socket to the above connection
-        setSocket(socket)
+  useEffect(() => {
+    //here if we have a authenticated user
+    if (authUser) {
+      //then we will create a connection with io method
+      const socket = io("http://localhost:5000", {
+        //when we are logged in we would like to take userId, for that we that as an argument
+        query: {
+          userId: authUser._id,
+        },
+      });
 
-        //socket.on() is used to listen to the events, it can be used both on client and server side
-        //once we are connected we would like to see who is connected
-        //this will return us users
-        socket.on("getOnlineUsers", (users) => {
-          setOnlineUsers(users)
-        })
+      //once the connection is completed, set the socket to the above connection
+      setSocket(socket);
 
-        //clean up function, clean our socket once the component is unmounted
-        return () => socket.close()
-      } else {
-        //if there is no authenticated user then we want to close the existing socket connection.
-        if (socket) {
-          socket.close()
-          setSocket(null)
-        }
+      //socket.on() is used to listen to the events, it can be used both on client and server side
+      //once we are connected we would like to see who is connected
+      //this will return us users
+      socket.on("getOnlineUsers", (users) => {
+        setOnlineUsers(users);
+      });
+
+      //clean up function, clean our socket once the component is unmounted
+      return () => socket.close();
+    } else {
+      //if there is no authenticated user then we want to close the existing socket connection.
+      if (socket) {
+        socket.close();
+        setSocket(null);
       }
-    }, [authUser])
-    
+    }
+  }, [authUser]);
 
-    return <SocketContext.Provider value={{ socket, onlineUsers }}>{children}</SocketContext.Provider>
-}
+  return (
+    <SocketContext.Provider value={{ socket, onlineUsers }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
